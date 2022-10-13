70e53f885c20bc67c5c53ca3dfb1ecdd9d47eecb
Sam Avery <sam.avery1@gmail.com>
2022-10-12 20:16:08 -0500

    commit tree 15b6d92666573b9c583c4d0ad84bf1e8d426ffc7

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+class DeliCounter
+    katz_deli = []
+
+    def line
+        if katz_deli.size > 0
+            katz.deli[index] +=1
+        else
+        "The line is currently empty."
+        end
+    end
+
+end
